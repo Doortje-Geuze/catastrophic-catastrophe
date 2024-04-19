247420fc84825001f105fbfada13e36c54c33d9c
Senna de Vries <senna.de.vries@hva.nl>
2024-04-19 15:26:18 +0200

    enemies bewegen naar het midden toe

--- game/GameObjects/RedEnemy.cs
+++ game/GameObjects/RedEnemy.cs
@@ -7,12 +7,16 @@ namespace Blok3Game.GameObjects
     public class RedEnemies : Enemies
     {
         private int EnemyHp;
-        private int EnemySpeed;
-        public RedEnemies(int hp, int speed, Vector2 position, string assetName = "Images/Enemies/standaard enemy") : base(hp, speed, position, assetName)
+        public double EnemySpeed;
+        public double XPosition { get; set; }
+        public double YPosition { get; set; }
+        public RedEnemies(int hp, double speed, Vector2 position, string assetName = "Images/Enemies/standaard enemy") : base(hp, speed, position, assetName)
         {
             EnemyHp = hp;
-            EnemySpeed = speed;
+            EnemySpeed = (double)speed;
             Position = position;
+            XPosition = (double)position.X;
+            YPosition = (double)position.Y;
         }
     }
 }
--- game/GameStates/GameState.cs
+++ game/GameStates/GameState.cs
@@ -1,24 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using Blok3Game.Engine.GameObjects;
 using Blok3Game.GameObjects;
+using Microsoft.VisualBasic.FileIO;
+using Microsoft.Xna.Framework;
 
 namespace Blok3Game.GameStates
 {
     public class GameState : GameObjectList
     {
-        private RedEnemies redEnemies;
+        private List<RedEnemies> redEnemiesList;
+
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var redEnemy in redEnemiesList)
+            {
+                if(redEnemy.XPosition >= 375)
+                {
+                    redEnemy.XPosition -= redEnemy.EnemySpeed;
+                    redEnemy.Position = new Microsoft.Xna.Framework.Vector2((float)redEnemy.XPosition, (float)redEnemy.YPosition);
+                }
+                if(redEnemy.XPosition <= 375)
+                {
+                    redEnemy.XPosition += redEnemy.EnemySpeed;
+                    redEnemy.Position = new Microsoft.Xna.Framework.Vector2((float)redEnemy.XPosition, (float)redEnemy.YPosition);
+                }
+                if(redEnemy.YPosition >= 225)
+                {
+                    redEnemy.YPosition -= redEnemy.EnemySpeed;
+                    redEnemy.Position = new Microsoft.Xna.Framework.Vector2((float)redEnemy.XPosition, (float)redEnemy.YPosition);
+                }
+                if(redEnemy.YPosition <= 225)
+                {
+                    redEnemy.YPosition += redEnemy.EnemySpeed;
+                    redEnemy.Position = new Microsoft.Xna.Framework.Vector2((float)redEnemy.XPosition, (float)redEnemy.YPosition);
+                }
+            }   
+        }
         public GameState() : base()
         {
+            redEnemiesList = new List<RedEnemies>();
 
             Random random = new Random();
 
             int swap = 0;
 
-            for(int i = 0; i < 200; i++)
+            for(int i = 0; i < 20; i++)
             {
                     int XPosition, YPosition;
                          
+                    
                     XPosition = random.Next(0, 750);
                     YPosition = random.Next(0, 550); 
                     
@@ -37,13 +71,13 @@ namespace Blok3Game.GameStates
                         }
                         
                     } while (XPosition >= 50 && XPosition <= 700 && YPosition >= 50 && YPosition <= 500);
-
-                    
             
             
-            redEnemies = new RedEnemies(100, 5, new Vector2(XPosition, YPosition));
-            Add(redEnemies);
+            RedEnemies redEnemy = new RedEnemies(100, 0.5, new Microsoft.Xna.Framework.Vector2(XPosition, YPosition));
+            redEnemiesList.Add(redEnemy);
+            Add(redEnemy);
             }
         }
     }
+    
 }
