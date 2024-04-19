12bf729885bed44f6d64c4d95214fd320443a85a
Tijn <tijnstuif@gmail.com>
2024-04-19 12:03:59 +0200

    border of the game set so player can't move outside of it

--- game/GameObjects/player.cs
+++ game/GameObjects/player.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using Blok3Game.Engine.GameObjects;
 using Blok3Game.Engine.Helpers;
 using Microsoft.Xna.Framework.Input;
@@ -9,6 +11,8 @@ public class Player : GameObjectList
     protected int HP;
     protected int X;
     protected int Y;
+    private int MoveSpeed = 5;
+    private int DistanceFromBorder = 20;
 
     public Player(int X, int Y, int Health) : base()
     {
@@ -22,21 +26,22 @@ public class Player : GameObjectList
     public override void HandleInput(InputHelper inputHelper)
     {
         base.HandleInput(inputHelper);
-        if (inputHelper.IsKeyDown(Keys.W))
+        if (inputHelper.IsKeyDown(Keys.W) && player.Position.Y > 0)
         {
-            Position = new Microsoft.Xna.Framework.Vector2(Position.X, Position.Y - 5);
+            player.Position = new Microsoft.Xna.Framework.Vector2(player.Position.X, player.Position.Y - MoveSpeed);
         }
-        if (inputHelper.IsKeyDown(Keys.A))
+        if (inputHelper.IsKeyDown(Keys.A) && player.Position.X > 0)
         {
-            Position = new Microsoft.Xna.Framework.Vector2(Position.X - 5, Position.Y);
+            player.Position = new Microsoft.Xna.Framework.Vector2(player.Position.X - MoveSpeed, player.Position.Y);
         }
-        if (inputHelper.IsKeyDown(Keys.S))
+        if (inputHelper.IsKeyDown(Keys.S) && player.Position.Y < 410)
         {
-            Position = new Microsoft.Xna.Framework.Vector2(Position.X, Position.Y + 5);
+            player.Position = new Microsoft.Xna.Framework.Vector2(player.Position.X, player.Position.Y + MoveSpeed);
         }
-        if (inputHelper.IsKeyDown(Keys.D))
+        if (inputHelper.IsKeyDown(Keys.D) && player.Position.X < 610)
         {
-            Position = new Microsoft.Xna.Framework.Vector2(Position.X + 5, Position.Y);
+            player.Position = new Microsoft.Xna.Framework.Vector2(player.Position.X + MoveSpeed, player.Position.Y);
         }
+        Console.WriteLine(player.Position);
     }
 }
