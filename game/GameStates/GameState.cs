d4dd0126b690111e1a5c1781c277f15486fcff32
Senna de Vries <senna.de.vries@hva.nl>
2024-04-21 15:25:00 +0200

    Enemies branch naar dev

--- game/GameStates/GameState.cs
+++ game/GameStates/GameState.cs
@@ -1,14 +1,103 @@
-﻿using System;
+using System;
+
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using Blok3Game.Engine.GameObjects;
+using Blok3Game.GameObjects;
+using Microsoft.VisualBasic.FileIO;
+using Microsoft.Xna.Framework;
+
+
+
 
 namespace Blok3Game.GameStates
 {
     public class GameState : GameObjectList
     {
+        //Lijst met alle enemies
+        private List<RedEnemies> redEnemiesList;
+
+
+        public override void Update(GameTime gameTime)
+        {
+            //Loop door de lijst met enemies
+            foreach (var redEnemy in redEnemiesList)
+            {
+
+                //If-statements om te checken wat de positie van de enemies is ten opzichte van een bepaald punt
+                if(redEnemy.XPosition >= 375)
+                {
+                    redEnemy.XPosition -= redEnemy.EnemySpeed;
+                    redEnemy.Position = new Microsoft.Xna.Framework.Vector2((float)redEnemy.XPosition, (float)redEnemy.YPosition);
+                }
+                if(redEnemy.XPosition <= 375)
+                {
+                    redEnemy.XPosition += redEnemy.EnemySpeed;
+                    redEnemy.Position = new Microsoft.Xna.Framework.Vector2((float)redEnemy.XPosition, (float)redEnemy.YPosition);
+                }
+                if(redEnemy.YPosition >= 225)
+                {
+                    redEnemy.YPosition -= redEnemy.EnemySpeed;
+                    redEnemy.Position = new Microsoft.Xna.Framework.Vector2((float)redEnemy.XPosition, (float)redEnemy.YPosition);
+                }
+                if(redEnemy.YPosition <= 225)
+                {
+                    redEnemy.YPosition += redEnemy.EnemySpeed;
+                    redEnemy.Position = new Microsoft.Xna.Framework.Vector2((float)redEnemy.XPosition, (float)redEnemy.YPosition);
+                }
+            }   
+        }
         public GameState() : base()
+
+        {
+
+            //Aanmaken van een nieuwe lijst
+            redEnemiesList = new List<RedEnemies>();
+
+            Random random = new Random();
+
+            int swap = 0;
+
+            //For-loop om meerdere enemies aan te maken
+            for(int i = 0; i < 20; i++)
+            {
+                    int XPosition, YPosition;
+                         
+                    //Willekeurige posities waar de enemies spawnen
+                    XPosition = random.Next(0, 750);
+                    YPosition = random.Next(0, 550); 
+                    
+
+                    //Do-While loop die ervoor zorgt dat de enemies aan de buiten randen spawnen 
+                    //De swap variabele zorgt ervoor dat de enemies evenredig worden verdeel aan alle kanten
+                    do{
+                        
+                        if(swap % 2 == 0)
+                        {
+                            XPosition = random.Next(0, 750);
+                            swap++;
+                        } 
+                        else
+                        {
+                            YPosition = random.Next(0, 550);
+                            swap++;
+                        }
+                        
+                    } while (XPosition >= 50 && XPosition <= 700 && YPosition >= 50 && YPosition <= 500);
+            
+            //Aanmaken van de enemies
+            RedEnemies redEnemy = new RedEnemies(100, 0.5, new Microsoft.Xna.Framework.Vector2(XPosition, YPosition));
+            redEnemiesList.Add(redEnemy);
+            Add(redEnemy);
+            }
+        }
+
         {           
             Player player = new(0, 0, 5);
             Add(player);
         }        
+
     }
+    
 }
