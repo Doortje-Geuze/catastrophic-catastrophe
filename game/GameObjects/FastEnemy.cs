85cc05f3ccad5bd6f309f2cca445db54387914fa
Doortje Geuze <doortje.geuze@hva.nl>
2024-05-21 18:18:09 +0200

    fast enemy clas (cucumber >:)) toegevoegd + image

---
+++ game/GameObjects/FastEnemy.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+
+namespace Blok3Game.GameObjects
+{
+    public class FastEnemy : Enemy
+    {
+        public int EnemyHitPoints;
+        
+        public FastEnemy(int hitPoints, int moveSpeed, Vector2 position) : base(hitPoints, moveSpeed, position, "Images/Characters/FastEnemy", 0, " ", 0)
+        {
+            EnemyHitPoints = hitPoints;
+            EnemyMoveSpeed = moveSpeed;
+        }
+    }
+}
