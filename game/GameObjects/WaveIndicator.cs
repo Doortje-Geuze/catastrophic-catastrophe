446a48610fbd8d2303a21ed5d675711a4bf2a87c
Sed van Hesselingen <sed.van.hesselingen@hva.nl>
2024-05-22 20:15:51 +0200

    Wave indicator + Background + Retry + Waves redone

---
+++ game/GameObjects/WaveIndicator.cs
@@ -0,0 +1,15 @@
+using Blok3Game.Engine.GameObjects;
+using Microsoft.Xna.Framework;
+
+namespace Blok3Game.GameObjects
+{
+    public class WaveIndicator : SpriteGameObject
+    {
+        private Vector2 Offset;
+        public WaveIndicator(Vector2 position, int sheetIndex) : base("Images/UI/Waves/waveSheet@1x3", 2, " " , sheetIndex)
+        {
+            Offset = new Vector2(Width / 2, Height / 2);
+            Position = position - Offset;
+        }
+    }
+}
