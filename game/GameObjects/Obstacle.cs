313f3ae75ff8dd115bf83411dfd932a1b719232c
Doortje Geuze <doortje.geuze@hva.nl>
2024-05-28 11:29:30 +0200

    obstacle class toegevoegd

---
+++ game/GameObjects/Obstacle.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+using Blok3Game.Engine.GameObjects;
+using Blok3Game.GameObjects;
+using Microsoft.Xna.Framework;
+
+namespace Blok3Game.SpriteGameObjects
+{
+abstract public class Obstacle : SpriteGameObject
+{
+    public Obstacle(Vector2 position, string assetName = " ", int layer = 0, string id = "", int sheetIndex = 0) : base(assetName)
+    {
+        Position = position;
+    }
+}
+
+public class GreenObstacle : Obstacle
+{
+
+public GreenObstacle(Vector2 position) : base(position, "Images/", 0, " ", 0)
+    {
+        
+
+    }
+}
+
+}
