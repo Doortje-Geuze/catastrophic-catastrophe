0a2cacc982f516283e19fe15c0e78e6b334bc229
Tijn <tijn.stuifbergen@hva.nl>
2024-05-30 10:07:06 +0200

    fixed bug with currency resetting after a game

--- game/GameStates/GameState.cs
+++ game/GameStates/GameState.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Blok3Game.Engine.GameObjects;
@@ -501,7 +501,6 @@ namespace Blok3Game.GameStates
             {
                 toRemoveList.Add(currency);
             }
-            player.currencyCounter = 0;
             playerCurrency.Text = $"you collected {player.currencyCounter} currency";
         }
     }
